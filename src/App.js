77164b970a6c36408e8534df98b6b22dac3d9f81
iamparul <himanshu@toothsi.in>
2022-04-18 12:21:40 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from "react";
 import "./App.css";
 import ClassCounter from "./components/ClassCounter";
 import HookCounter from "./components/HookCounter";
+import HookCounterThree from "./components/HookCounterThree";
+import HookCounterTwo from "./components/HookCounterTwo";
+import HookCounterFour from "./components/HookCounterFour";
 
 function App() {
   return (
     <div className="App">
-      <ClassCounter />
+      {/* <ClassCounter />
       <HookCounter />
+      <HookCounterTwo />  
+      <HookCounterThree />*/}
+      <HookCounterFour />
     </div>
   );
 }
